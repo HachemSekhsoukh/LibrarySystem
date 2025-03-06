ff0659bbd6c4a4a407cd5e8d77387ae076f7465d
Mesaaoud-Nacer Rafik <messaoudnacerr@gmail.com>
2025-03-06 17:30:08 +0100

    table template initial

--- src/Pages/readers.jsx
+++ src/Pages/readers.jsx
@@ -1,7 +1,45 @@
+import React from "react";
+import Table from "../components/Table";
+import "../CSS/readers.css";
 
 const Readers = () => {
+  const columns = [
+    { label: "ID", key: "id" },
+    { label: "Family Name", key: "familyName" },
+    { label: "First Name", key: "firstName" },
+    { label: "Date of Birth", key: "dob" },
+    { label: "Status", key: "status" },
+    { label: "Category", key: "category" },
+    { label: "Inscription Date", key: "inscriptionDate" },
+  ];
+
+  const data = [
+    {
+      id: "222235346620",
+      familyName: "GUERGOUR",
+      firstName: "Youcef",
+      dob: "07/05/2004",
+      status: "NaN",
+      category: "3rd Year",
+      inscriptionDate: "19/09/2022",
+    },
+    {
+      id: "222235346621",
+      familyName: "MESSAOUD",
+      firstName: "Nacer",
+      dob: "15/03/2003",
+      status: "Active",
+      category: "4th Year",
+      inscriptionDate: "10/08/2021",
+    },
+  ];
+
   return (
-    <></>
+    <div className="readers-page">
+        <div className="table-container">
+        <Table columns={columns} data={data} showActions={true} />
+        </div>  
+    </div>
   );
 };
 
