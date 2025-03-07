ba101bb7ea710b6aeb5fdf1724c3abe2d97f8bf5
HachemSekhsoukh <hachem.safi.eddine.sekhsoukh@ensia.edu.dz>
2025-03-08 00:10:27 +0100

    Update catalogage.jsx

--- src/Pages/catalogage/catalogage.jsx
+++ src/Pages/catalogage/catalogage.jsx
@@ -1,7 +1,48 @@
+import React from "react";
+import Table from "../../components/Table";
+import "../../CSS/readers.css";
+import Button from '../../components/Button';
 
 const Catalogage = () => {
+  const columns = [
+    { label: "ID", key: "id" },
+    { label: "Title", key: "title" },
+    { label: "Author", key: "author" },
+    { label: "Type", key: "type" },
+    { label: "Status", key: "status" },
+    { label: "ISBN", key: "isbn" },
+  ];
+  const data = [
+    {
+      id: "222235346620",
+      title: "GUERGOUR",
+      author: "Youcef",
+      status: "Available",
+      isbn: "978-2-1234-5680-3",
+      type: "Book",
+    },
+    {
+      id: "222235346620",
+      title: "GUERGOUR",
+      author: "Youcef",
+      status: "Available",
+      isbn: "978-2-1234-5680-3",
+      type: "Book",
+    },
+  ];
+
     return (
-      <></>
+      <div className="books-page">
+          <div className="container">
+            <div id="table">
+            <Table columns={columns} data={data} showActions={true} />
+            </div>
+            <div className="bottom-buttons">
+              <Button onClick = {() => {}} label={"Import Books"} lightBackgrnd={true}></Button>
+              <Button onClick = {() => {}} label={"Add New Book"} lightBackgrnd={false}></Button>
+            </div>
+          </div>
+      </div>
     );
   };
   
