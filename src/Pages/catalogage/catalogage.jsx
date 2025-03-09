cf1428aa48d3e9bd5b19109fb9e30f08aa54723b
HachemSekhsoukh <hachem.safi.eddine.sekhsoukh@ensia.edu.dz>
2025-03-09 02:24:57 +0100

    adding resources works

--- src/Pages/catalogage/catalogage.jsx
+++ src/Pages/catalogage/catalogage.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "../../components/Table";
 import Button from '../../components/Button';
 import Popup from "../../components/Popup";
 import "../../CSS/form.css";
+import "./resource_form";
 import { TextField, MenuItem, Grid } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Cancel';
+import AddResourceForm from "./resource_form";
 
 const Catalogage = () => {
   const [openPopup, setOpenPopup] = useState(false);
@@ -19,14 +21,62 @@ const Catalogage = () => {
     issn: "",
   });
 
+  const [resourceTypes, setResourceTypes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/resource-types")  // Ensure full URL with http://
+      .then(res => res.json())
+      .then(data => {
+        setResourceTypes(data);
+      })
+      .catch(error => console.error("API Error:", error));
+  }, []);
+  
+
   const handleChange = (e) => {
     setBookData({ ...bookData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = () => {
-    console.log("New Book Data:", bookData);
-    setOpenPopup(false); // Close modal after saving
+  const handleAddResource = async () => {
+    try {
+      const formattedData = {
+        r_inventoryNum: bookData.inventoryNum,
+        r_title: bookData.title,
+        r_author: bookData.author,
+        r_editor: bookData.editor,
+        r_edition: bookData.edition,
+        r_editionDate: bookData.editionDate,
+        r_editionPlace: bookData.editionPlace,
+        r_ISBN: bookData.isbn,
+        r_price: bookData.price,
+        r_cote: bookData.cote,
+        r_receivingDate: bookData.receivingDate,
+        r_status: bookData.status,
+        r_observation: bookData.observation,
+        r_type: bookData.type,
+      };
+  
+      const response = await fetch("http://localhost:5000/api/resources", { // Ensure correct API URL
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formattedData),
+      });
+  
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`HTTP ${response.status}: ${errorText}`);
+      }
+  
+      const result = await response.json();
+      alert("Resource added successfully!");
+      setOpenPopup(false);
+    } catch (error) {
+      alert(`Failed to add resource: ${error.message}`);
+    }
   };
+  
+
+
   const columns = [
     { label: "ID", key: "id" },
     { label: "Title", key: "title" },
@@ -35,6 +85,7 @@ const Catalogage = () => {
     { label: "Status", key: "status" },
     { label: "ISBN", key: "isbn" },
   ];
+
   const data = [
     {
       id: "222235346620",
@@ -54,110 +105,39 @@ const Catalogage = () => {
     },
   ];
 
-    return (
-      <div className="books-page">
-          <div className="container">
-            <div id="table">
-            <Table columns={columns} data={data} showActions={true} />
-            </div>
-            <div className="bottom-buttons">
-              <Button 
-                onClick={() => {}} 
-                label="Import Books" 
-                lightBackgrnd={true} 
-                icon={<FileUploadIcon />}
-                size="large"
-              />
-              <Button 
-                onClick={() => setOpenPopup(true)} 
-                label="Add New Book" 
-                lightBackgrnd={false}
-                icon={<AddIcon />}
-                size="large"
-              />
-            </div>
-          </div>
-          <Popup title="Add Book" openPopup={openPopup} setOpenPopup={setOpenPopup}>
-          <Grid container spacing={2}>
-                    {/* Document Type */}
-                    <Grid item xs={6}>
-                        <TextField
-                            select
-                            label="Document Type"
-                            name="type"
-                            value={bookData.type}
-                            onChange={handleChange}
-                            variant="outlined"
-                            className="text-field"
-                            InputLabelProps={{ shrink: true }}
-                        >
-                            <MenuItem value="Book">Book</MenuItem>
-                            <MenuItem value="Research">Research Paper</MenuItem>
-                        </TextField>
-                    </Grid>
-
-                    {/* Author */}
-                    <Grid item xs={6}>
-                        <TextField
-                            label="Author"
-                            name="author"
-                            value={bookData.author}
-                            onChange={handleChange}
-                            variant="outlined"
-                            className="text-field"
-                            InputLabelProps={{ shrink: true }}
-                        />
-                    </Grid>
-
-                    {/* Title */}
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Title"
-                            name="title"
-                            value={bookData.title}
-                            onChange={handleChange}
-                            variant="outlined"
-                            className="text-field"
-                            InputLabelProps={{ shrink: true }}
-                        />
-                    </Grid>
-
-                    {/* ISBN */}
-                    <Grid item xs={6}>
-                        <TextField
-                            label="ISBN"
-                            name="isbn"
-                            value={bookData.isbn}
-                            onChange={handleChange}
-                            variant="outlined"
-                            className="text-field"
-                            InputLabelProps={{ shrink: true }}
-                        />
-                    </Grid>
-
-                    {/* ISSN */}
-                    <Grid item xs={6}>
-                        <TextField
-                            label="ISSN"
-                            name="issn"
-                            value={bookData.issn}
-                            onChange={handleChange}
-                            variant="outlined"
-                            className="text-field"
-                            InputLabelProps={{ shrink: true }}
-                        />
-                    </Grid>
-                </Grid>
-
-
-                {/* Buttons */}
-                <div className="form-buttons">
-                    <Button onClick={() => setOpenPopup(false)} label="Cancel" lightBackgrnd={true} />
-                    <Button onClick={() => setOpenPopup(false)} label="Add" lightBackgrnd={false} />
-                </div>
-          </Popup>
+  return (
+    <div className="books-page">
+      <div className="container">
+        <div id="table">
+          <Table columns={columns} data={data} showActions={true} />
+        </div>
+        <div className="bottom-buttons">
+          <Button
+            onClick={() => {}}
+            label="Import Books"
+            lightBackgrnd={true}
+            icon={<FileUploadIcon />}
+            size="large"
+          />
+          <Button
+            onClick={() => setOpenPopup(true)}
+            label="Add New Book"
+            lightBackgrnd={false}
+            icon={<AddIcon />}
+            size="large"
+          />
+        </div>
       </div>
-    );
-  };
-  
-  export default Catalogage;
+      <Popup title="Add Book" openPopup={openPopup} setOpenPopup={setOpenPopup}>
+        <AddResourceForm bookData={bookData} handleChange={handleChange} resourceTypes={resourceTypes} />
+        {/* Buttons */}
+        <div className="form-buttons">
+          <Button onClick={() => setOpenPopup(false)} label="Cancel" lightBackgrnd={true} />
+          <Button onClick={handleAddResource} label="Add" lightBackgrnd={false} />
+        </div>
+      </Popup>
+    </div>
+  );
+};
+
+export default Catalogage;
---
+++ src/Pages/catalogage/resource_form.jsx
@@ -0,0 +1,208 @@
+import { Grid, TextField, MenuItem } from "@mui/material";
+
+const AddResourceForm = ({ bookData, handleChange, resourceTypes }) => {
+    return (
+        
+        <Grid container spacing={2}>
+            {/* Inventory Number */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Inventory Number"
+                    name="inventoryNum"
+                    value={bookData.inventoryNum}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Document Type */}
+            <Grid item xs={6}>
+                <TextField
+                    select
+                    label="Document Type"
+                    name="type"
+                    value={bookData.type}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                >
+                    {resourceTypes.map((type) => (
+                        <MenuItem key={type.id} value={type.id}>
+                            {type.name}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </Grid>
+
+            {/* Title */}
+            <Grid item xs={12}>
+                <TextField
+                    label="Title"
+                    name="title"
+                    value={bookData.title}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Author */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Author"
+                    name="author"
+                    value={bookData.author}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Editor */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Editor"
+                    name="editor"
+                    value={bookData.editor}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Edition */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Edition"
+                    name="edition"
+                    value={bookData.edition}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Edition Date */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Edition Date"
+                    name="editionDate"
+                    type="date"
+                    value={bookData.editionDate}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Edition Place */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Edition Place"
+                    name="editionPlace"
+                    value={bookData.editionPlace}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* ISBN */}
+            <Grid item xs={6}>
+                <TextField
+                    label="ISBN"
+                    name="isbn"
+                    value={bookData.isbn}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Price */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Price"
+                    name="price"
+                    type="number"
+                    value={bookData.price}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Cote */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Cote"
+                    name="cote"
+                    value={bookData.cote}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Receiving Date */}
+            <Grid item xs={6}>
+                <TextField
+                    label="Receiving Date"
+                    name="receivingDate"
+                    type="date"
+                    value={bookData.receivingDate}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+
+            {/* Status */}
+            <Grid item xs={6}>
+                <TextField
+                    select
+                    label="Status"
+                    name="status"
+                    value={bookData.status}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
+                >
+                    <MenuItem value={1}>Available</MenuItem>
+                    <MenuItem value={0}>Unavailable</MenuItem>
+                </TextField>
+            </Grid>
+
+            {/* Observation */}
+            <Grid item xs={12}>
+                <TextField
+                    label="Observation"
+                    name="observation"
+                    value={bookData.observation}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                    multiline
+                    rows={3}
+                    InputLabelProps={{ shrink: true }}
+                />
+            </Grid>
+        </Grid>
+    );
+};
+
+export default AddResourceForm;
